38c737d62a72446d615ec78c1abd53c9ae8f34b6
dkgus12dkgus <dkgus12dkgus@naver.com>
2021-10-28 22:26:36 +0900

    sidebar,otherpage

--- src/App.js
+++ src/App.js
@@ -1,111 +1,19 @@
 import Topbar from "./components/topbar/Topbar.jsx" 
+import Sidebar from "./components/sidebar/Sidebar.jsx" 
+import "./app.css"
+
 
 function App() {
   return (
     <div>
       <Topbar/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-      test <br/>
-     
 
+      <div className="container">
+        <Sidebar/>
+        <div className="others">Other Pages</div>
       </div>
+
+    </div>
   );
 }
 
